98dedbf11918dc2658dfac3254cc90891807cb8f
Skitbet <54096334+Skitbet@users.noreply.github.com>
2022-09-15 14:14:42 -0400

    Update buildtools.bat

--- buildtools.bat
+++ buildtools.bat
@@ -1,14 +1,12 @@
 @echo off
+title SpigotMC BuildTools Builder
 IF NOT EXIST BuildTools (
     mkdir BuildTools
 )
 cd BuildTools
 curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 set /p Input=Enter the version: || set Input=latest
-java -jar BuildTools.jar --rev %Input%
-
-git config --global user.email "INSERT_EMAIL_HERE"
-git config --global commit.gpgsign true
-git config advice.addIgnoredFile false
-
+"path to java" -jar BuildTools.jar --rev %Input%
+cls
+echo "Done!"
 pause
