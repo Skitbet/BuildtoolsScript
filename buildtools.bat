30316db7bec3c4f416e08466a53a613dfa55cd48
Skitbet <54096334+Skitbet@users.noreply.github.com>
2023-09-23 18:21:20 -0400

    Update buildtools.bat

--- buildtools.bat
+++ buildtools.bat
@@ -1,13 +1,14 @@
 @echo off
 title SpigotMC BuildTools Builder
 set /p Version=Enter the version: || set Version=latest
-set /p Path=Path to java:
-IF NOT EXIST %Version% (
-    mkdir %Version%
+set /p JavaPath=Path to java:
+
+IF NOT EXIST "%Version%" (
+    mkdir "%Version%"
 )
-cd %Version%
+cd "%Version%"
 curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-%Path% -jar BuildTools.jar --rev %Version%
+"%JavaPath%" -jar BuildTools.jar --rev %Version%
 cls
-echo "Done!"
+echo Done!
 pause
